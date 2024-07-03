2123b75132b1b1bf42d7cd21d9caf6f1a0c31d29
Brijesh Patel <brijeshpatel96@gmail.com>
2024-07-03 16:59:19 +0100

    adding initial files and navbar

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Navbar from './components/Navbar'
+import { Route, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <>
+       <Navbar />
+
+    </>
+  )
+}
+
+export default App
