3fd5456740859025e5d6c3892a72a71a9ba7a317
Brijesh Patel <brijeshpatel96@gmail.com>
2024-07-04 10:23:38 +0100

    add header main.js file

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import Navbar from './components/Navbar'
+import Main from './components/Main'
 import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <>
        <Navbar />
+       <Main />
 
     </>
   )
